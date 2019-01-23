bcb0e83ec6a8e920ec9c994a5023eb7f428c711e
Jamal Al Mlah <9907jaal@res.ludvika.intra>
2019-01-23 09:37:17 +0100

    första controller

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
+
 const express = require('express');
 const app = express ();
+app.get('/', (request, response) => {
+  response.send('Hello world');
+});
 app.listen(1111, () => console.log('Application running on port 1111'));
