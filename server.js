f0e25a3713e652e1905d7bd4f2d2af41a812a33b
Jamal Al Mlah <9907jaal@res.ludvika.intra>
2019-01-29 15:57:23 +0100

    29/01

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const handlebars = require("express-handlebars");
 const app = express();
-app.use('/static', express.static('public'));
+app.use("/static", express.static("public"));
 app.engine("handlebars", handlebars({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
@@ -11,5 +11,13 @@ app.get("/", (request, response) => {
 app.get("/sida2", (request, response) => {
   response.render("second");
 });
-
+app.get("/skapaerbjudande", (request, response) => {
+  response.render("skapaerbjudande");
+});
+app.get("/company", (request, response) => {
+  response.render("company");
+});
+app.get("/controlpanel/deals", (request, response) => {
+response.render("controlpaneldeals");
+});
 app.listen(1111, () => console.log("Application running on port 1111"));
