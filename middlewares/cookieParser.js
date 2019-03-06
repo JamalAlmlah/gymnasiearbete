287fc574793fcdee4d75acb647ac47ab6334c329
Jamal Al Mlah <9907jaal@res.ludvika.intra>
2019-03-06 11:35:27 +0100

    give me feedback bby

---
+++ middlewares/cookieParser.js
@@ -0,0 +1,11 @@
+const querystring = require('querystring');
+const cookieParser = (request, respone, next) => {
+  if (request.headers.cookie){
+    request.cookies = querystring.parse(request.headers.cookie.replace(/; /g, '&' )); 
+
+  }
+  next()
+ 
+  
+};
+module.exports = cookieParser;
--- server.js
+++ server.js
@@ -1,13 +1,40 @@
 const express = require("express");
 const ObjectID = require("mongodb").ObjectID;
 const handlebars = require("express-handlebars");
-const bodyPaeser = require("./middlewares/bodyParser");
+const bodyParser = require("./middlewares/bodyParser");
+const cookieParser = require("./middlewares/cookieParser");
 const app = express();
 const connect = require("./models/connect");
-app.use(bodyPaeser);
+app.use(bodyParser);
+app.use(cookieParser);
 app.use("/static", express.static("public"));
 app.engine("handlebars", handlebars({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+
+app.post("/controlpanel/registrera", async (request, response) => {
+  const db = await connect();
+  const user = {
+    username: request.body.UserName,
+    email: request.body.Email,
+    password: request.body.Password
+  };
+    const collection = db.collection("users");
+  await collection.insertOne(user);
+  response.sendStatus(204);
+});
+
+app.post("/controlpanel/login", async (request, response) => {
+const db = await connect(); 
+const collection = db.collection("users");
+const login = await collection.find({username: request.body.UserName, password: request.body.Password}).toArray();
+if(login.length < 1) { 
+  response.render("login", { layout: "login", meddelande: "Fel användernamn eller lösenord" });
+} else {
+response.set('Set-Cookie', 'admin=true;');
+  response.redirect("/controlpanel")
+} 
+
+})
 
 app.post("/controlpanel/erbjudande/edit/:_id", async (request, response) => {
   const db = await connect();
@@ -38,7 +65,13 @@ app.get("/controlpanel/erbjudande", async (request, response) => {
   const db = await connect();
   const collection = db.collection("deals");
   const dealspost = await collection.find().toArray();
-  response.render("erbjudande", { deals: dealspost, layout: "cp" });
+  if(request.cookies.admin) {
+    response.render("erbjudande", { deals: dealspost, layout: "cp" });
+  } else {
+  
+    response.redirect("/controlpanel/login");
+  }
+  
 });
 app.post("/skapaerbjudande", async (request, response) => {
   const deals = {
@@ -55,22 +88,56 @@ app.post("/skapaerbjudande", async (request, response) => {
   const companycollection = db.collection("company");
   await companycollection.insertOne(company);
   response.redirect("/controlpanel/skapaerbjudande");
+
 });
 app.get("/controlpanel/skapaerbjudande", (request, response) => {
+response.set('Set-Cookie', 'user3=Jamal3; expires=Thu, 01 Jan 1970 00:00:00 GMT');
+console.log('request.headers.cookie', request.headers.cookie); 
+console.log('request.cookies', request.cookies);
+
+if(request.cookies.admin) {
   response.render("skapaerbjudande", { layout: "cp" });
+} else {
+
+  response.redirect("/controlpanel/login");
+}
+
+
 });
 app.get("/kontakta", (request, response) => {
   response.render("Kontakta");
 });
 
+app.get("/about", (request, response) => {
+  response.render("about");
+});
+app.get("/controlpanel/registrera", (request, response) => {
+  if(request.cookies.admin) {
+    response.render("registrera", {layout: "cp"});
+  } else {
+
+    response.redirect("/controlpanel/login");
+  }
+});
+
 app.get("/controlpanel", (request, response) => {
-  response.render("controlpanel", { layout: "cp" });
+  if(request.cookies.admin) {
+    response.render("controlpanel", { layout: "cp" });
+  } else {
+
+    response.redirect("/controlpanel/login");
+  }
+
 });
 app.get("/controlpanel/meddelande", (request, response) => {
-  response.render("meddelande", { layout: "cp" });
+  if(request.cookies.admin) {
+    response.render("meddelande", { layout: "cp" });
+  } else {
+    response.redirect("/controlpanel/login");
+  }
 });
 app.get("/controlpanel/login", (request, response) => {
-  response.render("login", { layout: "" });
+  response.render("login", { layout: "login" });
 });
 app.get("/company/:name", async (request, response) => {
   const name = request.params.name;
