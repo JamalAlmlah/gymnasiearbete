908cc07eb05cf8d199f26b48bbef49cce3069f6b
Jamal Al Mlah <9907jaal@res.ludvika.intra>
2019-04-03 11:00:05 +0200

    03/04

--- middlewares/bodyParser.js
+++ middlewares/bodyParser.js
@@ -1,19 +1,21 @@
-const querystring = require("querystring");
-const bodyParser = (request, respone, next) => {
-  if (
-    request.headers["content-type"] &&
-    request.headers["content-type"].includes("x-www-form-urlencoded")
-  ) {
-    let data = "";
-    request.on("data", chunk => {
-      data += chunk;
-    });
-    request.on("end", () => {
+const querystring = require('querystring');
+
+const bodyParser = (request, response, next) => {
+  let data = '';
+  request.on('data', chunk => {
+    data += chunk;
+  });
+
+  request.on('end', () => {
+    if (
+      request.headers['content-type'] &&
+      request.headers['content-type'].includes('x-www-form-urlencoded')
+    ) {
       data = querystring.parse(data);
-      request.body = data;
-      next();
-    });
-  }
-next();
+    }
+    request.body = data;
+    next();
+  });
 };
+
 module.exports = bodyParser;
